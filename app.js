aff052b69baca6471e807cc21551fcd4f03f56b3
Aravindh9490 <haravind123143@gmail.com>
2023-10-28 18:24:49 +0000

    user login and register

---
+++ app.js
@@ -0,0 +1,176 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const databasePath = path.join(__dirname, "userData.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+const validatePassword = (password) => {
+  return password.length > 4;
+};
+
+app.post("/register", async (request, response) => {
+  const { email, password, confirm_password } = request.body;
+  //   let hashedPassword = null;
+  //   if (password === confirm_password) {
+  //     hashedPassword = await bcrypt.hash(password, 10);
+  //   }
+
+  const selectUserQuery = `SELECT * FROM user WHERE email = '${email}';`;
+  const databaseUser = await database.get(selectUserQuery);
+
+  if (databaseUser === undefined && password === confirm_password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const createUserQuery = `
+     INSERT INTO
+      user (email, password)
+     VALUES
+      (
+       '${email}',
+       '${hashedPassword}'
+      );`;
+    if (validatePassword(password)) {
+      await database.run(createUserQuery);
+      response.send("User created successfully");
+    } else {
+      response.status(400);
+      response.send("Password is too short");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const { email, password } = request.body;
+  //   console.log(email, password);
+  const selectUserQuery = `SELECT * FROM user WHERE email = '${email}';`;
+  const databaseUser = await database.get(selectUserQuery);
+  //   console.log(databaseUser);
+
+  if (databaseUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      databaseUser.password
+    );
+    if (isPasswordMatched === true) {
+      response.send("Login success!");
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.put("/change-password", async (request, response) => {
+  const { email, oldPassword, newPassword } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE email = '${email}';`;
+  const databaseUser = await database.get(selectUserQuery);
+  if (databaseUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(
+      oldPassword,
+      databaseUser.password
+    );
+    if (isPasswordMatched === true) {
+      if (validatePassword(newPassword)) {
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        const updatePasswordQuery = `
+          UPDATE
+            user
+          SET
+            password = '${hashedPassword}'
+          WHERE
+            email = '${email}';`;
+
+        const user = await database.run(updatePasswordQuery);
+
+        response.send("Password updated");
+      } else {
+        response.status(400);
+        response.send("Password is too short");
+      }
+    } else {
+      response.status(400);
+      response.send("Invalid current password");
+    }
+  }
+});
+
+app.put("/user-details", async (request, response) => {
+  const {
+    username,
+    name,
+    gender,
+    location,
+    email,
+    age,
+    mobile_num,
+    dob,
+    full_address,
+  } = request.body;
+  console.log(email);
+
+  const selectUserQuery = `
+  UPDATE 
+  user
+  SET
+  username="${username}",
+  name="${name}",
+  gender="${gender}",
+  location="${location}",
+  age=${age},
+  mobile_num=${mobile_num},
+  dob="${dob}",
+  full_address="${full_address}"
+
+  WHERE
+  email="${email}";
+  `;
+  const result = await database.run(selectUserQuery);
+  const output = await database.get(
+    `SELECT * FROM user WHERE email="${email}";`
+  );
+
+  //   response.send("successfully added");
+  response.send({ output: output, statues: "successfully added" });
+});
+
+app.get("/all", async (req, res) => {
+  const sqlQuery = `select * from user;`;
+  const result = await database.all(sqlQuery);
+  res.send(result);
+});
+
+module.exports = app;
