0f339935c7bda47137ec7dc973bda8e0662a2977
Aravindh9490 <haravind123143@gmail.com>
2023-10-31 08:48:05 +0000

    corssss

--- backend/app.js
+++ backend/app.js
@@ -12,19 +12,7 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:3000"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 let database = null;
 
